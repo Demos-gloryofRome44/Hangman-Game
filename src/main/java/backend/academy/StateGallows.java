81d26017c3bb6bb3b109697b05bb83d6f8640ed3
Demos <166336733+Demos-gloryofRome44@users.noreply.github.com>
2024-09-14 01:17:28 +0300

    Initial commit of StateGallows class to represent the gallows state in the game

---
+++ src/main/java/backend/academy/StateGallows.java
@@ -0,0 +1,31 @@
+package backend.academy;
+
+import java.io.PrintStream;
+import static java.lang.System.out;
+
+public class StateGallows {
+    private static final String STAND = " ========\n";
+    private static final String ROOF = "+—————+\n";
+    private static final String LINE = "  |\n";
+    private static final String LINE2 = "  |   |\n";
+    private static final String HEAD = "  |   o \n";
+    private static final String BODY = "  |  /o\\ \n";
+    private static final String LENG = "  |  / \\ \n";
+    private static final String CHAIR = "  |  ===\n";
+    private static final String[] GALLOW_STATE = {
+        "\n"
+            + "\n"
+            + "\n"
+            + "\n"
+            + "\n"
+            + "\n"
+            + STAND,
+        LINE + LINE + LINE + LINE + LINE + LINE + STAND,
+        ROOF + LINE + LINE + LINE + LINE + LINE + STAND,
+        ROOF + LINE2 + LINE + LINE + LINE + LINE + CHAIR + STAND,
+        ROOF + LINE2 + HEAD + BODY + LENG + CHAIR + STAND,
+        ROOF + LINE2 + HEAD + BODY + LENG + LINE + STAND
+    };
+
+
+}
